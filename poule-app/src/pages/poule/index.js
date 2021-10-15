bbc72c36ea3c414c9305a679230af04b2b7cf544
Robin Galema <RGalwma99@gmail.com>
2021-10-15 13:26:54 +0200

    Listing all poules on poules page

---
+++ poule-app/src/pages/poule/index.js
@@ -0,0 +1,7 @@
+import pouleData from '.../data/poules.json';
+
+const Poule = (props) => {
+    
+}
+
+export default Poule;
--- poule-app/src/pages/poules/index.js
+++ poule-app/src/pages/poules/index.js
@@ -1,4 +1,13 @@
 import { useHistory } from "react-router";
+import pouleData from ".././../data/poules.json"
+
+const PouleOverview = (props) => {
+    let value = pouleData.poules.map( poule => {
+        return (<p>test {poule.id} | {props.user} </p>);
+    });
+
+    return value;
+}
 
 function Poules(props){
 
@@ -9,6 +18,10 @@ function Poules(props){
     return(
         <div className="page-content page-poules">
             <h2>This is the poules page</h2>
+
+            <div className="poules-container">
+                <PouleOverview user={props.user}/>
+            </div>
         </div>
     );
 }
